50f3633c7f89907cd1a95ed80357070bdc72a606
Trent Lim <trent_lim_nz@yahoo.com>
2024-10-30 23:13:31 +0100

    Create post endpoints for adding responses

--- EffectoryAssignment/Program.cs
+++ EffectoryAssignment/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Principal;
+using EffectoryAssignment.Constants;
 using EffectoryAssignment.Models;
 using Newtonsoft.Json;
 
@@ -44,13 +46,102 @@ app.MapGet("/questions", (int page = 1, int pageSize = 10) =>
         .Take(pageSize)
         .ToList();
 
-    return Results.Ok(questions);
+    var json = JsonConvert.SerializeObject(questions);
+    return Results.Ok(json);
 });
 
 app.MapGet("/questions/{id:int}", (int id) =>
 {
     var question = questionnaire.GetQuestionById(id);
-    return question is not null ? Results.Ok(question) : Results.NotFound();
+    var json = JsonConvert.SerializeObject(question);
+    return question is not null ? Results.Ok(json) : Results.NotFound($"Question {id} not found");
+});
+
+// For open-ended questions
+app.MapPost("questions/{questionId:int}/responses", (int questionId, int userId, string department, string text, string language) =>
+{
+    if (!Departments.ValidDepartments.Contains(department))
+    {
+        return Results.BadRequest($"Invalid department. Valid departments are: {string.Join(", ", Departments.ValidDepartments)}");
+    }
+
+    var question = questionnaire.GetQuestionById(questionId);
+    if (question is null)
+    {
+        return Results.NotFound($"Question {questionId} not found");
+    }
+
+    Answer? answer = question.QuestionnaireItems?.OfType<Answer>().First();
+    if (answer is null)
+    {
+        return Results.NotFound($"Answer not found");
+    }
+
+    // If it is an open-ended question and there is no text 
+    if (string.IsNullOrWhiteSpace(text))
+    {
+        return Results.BadRequest("Text is required for open-ended questions");
+    }
+
+    var texts = new Dictionary<string, string>();
+    texts.Add(language, text);
+
+    var response = new Response
+    {
+        AnswerType = 2,
+        UserId = userId,
+        Department = department,
+        Texts = texts,
+    };
+
+    var result = questionnaire.AddResponseToAnswer(response, answer);
+    if (result is null)
+    {
+        return Results.Problem("An error occured");
+    }
+    else
+    {
+        return Results.Ok();
+    }
+});
+
+// For multi-choice questions
+app.MapPost("questions/{questionId:int}/answers/{answerId:int}/responses", (int questionId, int answerId, int userId, string department) =>
+{
+    if (!Departments.ValidDepartments.Contains(department))
+    {
+        return Results.BadRequest($"Invalid department. Valid departments are: {string.Join(", ", Departments.ValidDepartments)}");
+    }
+
+    var question = questionnaire.GetQuestionById(questionId);
+    if (question is null)
+    {
+        return Results.NotFound($"Question {questionId} not found");
+    }
+
+    var answer = questionnaire.GetAnswerById(answerId);
+    if (answer is null)
+    {
+        return Results.NotFound($"Answer {answerId} not found");
+    }
+
+    var response = new Response
+    {
+        AnswerId = answerId,
+        AnswerType = 1,
+        UserId = userId,
+        Department = department,
+    };
+
+    var result = questionnaire.AddResponseToAnswer(response, answer);
+    if (result is null)
+    {
+        return Results.Problem("An error occured");
+    }
+    else
+    {
+        return Results.Ok();
+    }
 });
 
 app.Run();
