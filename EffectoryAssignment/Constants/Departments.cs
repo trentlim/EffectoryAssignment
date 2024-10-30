17a2d55d69a124ff0994986679bf4a075c01ba60
Trent Lim <trent_lim_nz@yahoo.com>
2024-10-30 21:24:18 +0100

    Create constants file for valid departments

---
+++ EffectoryAssignment/Constants/Departments.cs
@@ -0,0 +1,13 @@
+﻿namespace EffectoryAssignment.Constants
+{
+    public class Departments
+    {
+        public static readonly HashSet<string> ValidDepartments = new HashSet<string>
+        {
+            "marketing",
+            "sales",
+            "development",
+            "reception"
+        };
+    }
+}
